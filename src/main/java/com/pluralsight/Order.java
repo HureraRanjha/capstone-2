adf8eb1767e8b762ed92170f1d6ca90fcac5b0dc
HureraRanjha <hureraranjha9@gmail.com>
2026-05-28 12:50:47 -0400

    Implemented Order class and getting total order value.

---
+++ src/main/java/com/pluralsight/Order.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Order
+{
+    private ArrayList<Product> products = new ArrayList<Product>();
+
+    public void addProduct(Product product)
+    {
+        this.products.add(product);
+    }
+
+    public double getOrderValue()
+    {
+        double price = 0;
+        for(Product p: products)
+        {
+            price += p.getPrice();
+        }
+        return price;
+    }
+
+
+}
