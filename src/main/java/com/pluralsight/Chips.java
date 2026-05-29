9abda915d575649e7b81d2e3f20fc9359f6beb21
HureraRanjha <hureraranjha9@gmail.com>
2026-05-29 02:44:28 -0400

    Chips comments

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -2,12 +2,14 @@ package com.pluralsight;
 
 public class Chips implements Product
 {
+    //This is an overriden method to get the price of a chips bag which is always 1.50
     @Override
     public double getPrice()
     {
         return 1.5;
     }
 
+    //This toString Method helps output to the txt and console
     @Override
     public String toString()
     {
